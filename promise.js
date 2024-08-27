86159a288d6929918e13a762ea9f108d565eb099
Akhil Sai Polisetty <55974202+akhil3000@users.noreply.github.com>
2024-08-27 20:04:06 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -9,3 +9,5 @@ promise.then((res) => {
 }).catch(function(err){
     console.log(err.message);
 })
+//Callback hell is solved with promises when one callback is passed as argument to another callback promise object is used to ensure successfull implementation of
+//callback or rejection it will guarantee the completion of operation
